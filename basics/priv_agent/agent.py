2dd1f3e8a1ca54e1fb1b2318093e9e676b8eb235
ctxadm <kai.pauli@ccia.ch>
2026-05-25 11:43:21 +0200

    Update agent.py
    base_url=os.getenv("TTS_URL", "http://172.16.0.220:8881/v1"),

--- basics/priv_agent/agent.py
+++ basics/priv_agent/agent.py
@@ -613,7 +613,7 @@ async def entrypoint(ctx: JobContext):
         tts=openai.TTS(
             model="tts-1",
             voice="alloy",
-            base_url="http://172.16.0.220:8888/v1",
+            base_url=os.getenv("TTS_URL", "http://172.16.0.220:8881/v1"),
             api_key="sk-nokey",
             speed=1.05,
         ),
